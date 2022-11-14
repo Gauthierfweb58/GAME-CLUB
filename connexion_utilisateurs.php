95a350cc3cff5cdc254cd68e4a408eccc34b5d38
frederic.gauthier89@gmail.com <frederic.gauthier89@gmail.com>
2022-11-14 14:51:31 +0100

    ajout

--- connexion_utilisateurs.php
+++ connexion_utilisateurs.php
@@ -1,7 +1,13 @@
 <?php
+//On initialise la session PHP avec session_start() ;
 session_start();
+
+//Avec require_once nous vérifions si la page connexion_bdd.php a déjà été incluse et si c’est le cas il ne l’inclut pas une nouvelle fois.
 	require_once 'connexion_bdd.php';
 	
+	/*Ensuite avec une condition ISSET on vérifie si une variable est déclarée, que tout les champs requis sont remplis , 
+	/*suivie d’un bloc d’exceptions que l’on peut voir à travers TRY qui va entourer le code pour faciliter la saisie d’une exception potentielle
+	/* et d’un bloc CATCH qui définit comment réagir si un ou plusieurs types d’exceptions ou erreurs a été lancée.*/
 	if(isset($_POST['connecter'])){
 		if($_POST['email'] != "" || $_POST['mdp'] != ""){
 			$email = $_POST['email'];
@@ -13,6 +19,7 @@ session_start();
 			$fetch = $query->fetch();
 			if($row > 0) {
 				$_SESSION['user'] = $fetch['id'];
+				$_SESSION['pseudo'] = $fetch['pseudo'];
 				header("location: index.php");
 			} else{
 				echo "
